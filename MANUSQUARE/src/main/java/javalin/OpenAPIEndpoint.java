c50b97a3a08a36223df2ea25b4f846f2d0f6466d
Peter Haro <peter.haro@sintef.no>
2019-12-09 23:21:51 +0100

    Added docker support

--- MANUSQUARE/src/main/java/javalin/OpenAPIEndpoint.java
+++ MANUSQUARE/src/main/java/javalin/OpenAPIEndpoint.java
@@ -43,7 +43,7 @@ public class OpenAPIEndpoint {
         });
 
         app.post("/matching", SemanticMatchingController.PerformSemanticMatching);
-        app.start(5445);
+        app.start(7000);
 
 
 
